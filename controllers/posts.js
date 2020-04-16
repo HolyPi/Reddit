cb44afb8a992bd17236828afe7f20c82070cea29
HolyPi <joanelly2018@gmail.com>
2020-04-15 20:46:06 -0400

    Fixed test not passing

--- controllers/posts.js
+++ controllers/posts.js
@@ -9,7 +9,6 @@ module.exports = app => {
     app.post('/post/new', (req, res) => {
         // INSTANTIATE INSTANCE OF POST MODEL
         const post = new Post(req.body);
-        
         // SAVE INSTANCE OF POST MODEL TO DB
         post.save((err, post) => {
             console.log(err)
